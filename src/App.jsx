9b883ef0382487f00a51a8058d069d8024e33c7a
shajans <shajan072008@gmail.com>
2026-08-17 21:21:49 +0530

    main class added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-<main className="min-h-screen bg-slate-950 flex flex-col items-center justify-center p-6"> 
-  <div className="w-full max-w-md flex flex-col gap-6">
-    <header className="flex items-center justify-between px-2">
-      <section className="bg-slate-900/60 border border-slate-800 rounded-3xl shadow-2xl gap-8">
-        <nav>
-          
-        </nav>
-      </section>
-    </header>
-  </div>
-</main>
+function App() {
+  return(
+    <main className="min-h-screen bg-slate-950 text-slate-100 flex flex-col items-center justify-center p-6">
+      <div className="w-full max-w-md flex flex-col gap-6">
+        <header className="flex items-center justify-between px-2"> 
+          <h1 className="text-sm font-bold tracking-wider text-rose-500 uppercase flex items-center gap-2">
+            Focus
+          </h1>
+        </header>
+      </div>
+    </main>
+  )
+}
+export default App;
